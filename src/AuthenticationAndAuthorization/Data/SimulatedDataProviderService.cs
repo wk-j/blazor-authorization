053f715f11b498e0a92bf6a045a705756c5506e4
wk <somnuk.wk@gmail.com>
2022-12-06 14:59:54 +0700

    Update

---
+++ src/AuthenticationAndAuthorization/Data/SimulatedDataProviderService.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace AuthenticationAndAuthorization.Data
+{
+    public class SimulatedDataProviderService
+    {
+        public List<User> Users { get; set; } = new()
+        {
+            new()
+            {
+                Username = "User1",
+                Password = "User1",
+                Age = 19,
+                IsPremiumMember = false
+            },
+            new()
+            {
+                Username = "User2",
+                Password = "User2",
+                Age = 10,
+                IsPremiumMember = true
+            },
+            new()
+            {
+                Username = "User3",
+                Password = "User3",
+                Age = 20,
+                IsPremiumMember = true
+            }
+        };
+
+        public List<Toy> Toys { get; set; } = new()
+        {
+            new()
+            {
+                Name = "Car",
+                AgeRequired = 0
+            },
+            new()
+            {
+                Name = "Plastic knife",
+                AgeRequired = 15
+            }
+        };
+
+        public IEnumerable<string> GetUserRoles(User user)
+        {
+            var result = user.Username switch
+            {
+                "User1" => new List<string>() { "Admin", "User" },
+                "User2" => new List<string>() { "User" },
+                _ => new List<string>() { },
+            };
+
+            return result;
+        }
+    }
+}
---
+++ src/AuthenticationAndAuthorization/Startup.cs
@@ -0,0 +1,67 @@
+using AuthenticationAndAuthorization.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace AuthenticationAndAuthorization
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<SimulatedDataProviderService>();
+            services.AddScoped<WebsiteAuthenticator>();
+            services.AddScoped<AuthenticationStateProvider>(sp => sp.GetRequiredService<WebsiteAuthenticator>());
+            services.AddAuthorization(config =>
+            {
+                config.AddPolicy("CanBuyAlcohol", policy =>
+                {
+                    policy.AddRequirements(new AdultRequirement());
+                    policy.RequireClaim("IsPremiumMember", true.ToString());
+                });
+
+                config.AddPolicy("OverAge", policy => policy.AddRequirements(new OverAgeRequirement()));
+            });
+            services.AddSingleton<IAuthorizationHandler, AdultRequirementHandler>();
+            services.AddSingleton<IAuthorizationHandler, OverAgeRequirementHandler>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
